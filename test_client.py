63d2e49a4c982f66b90e7f30a038da0dfabad6e0
JdeH <info@qquick.org>
2019-01-01 15:18:24 +0100

    test expanded

--- test_client.py
+++ test_client.py
@@ -2,6 +2,7 @@ import asyncio
 import websockets
 import json
 import sys
+import aioconsole
 
 hostName = 'localhost'
 portNr = 8765
@@ -19,13 +20,15 @@ class TestClient:
         async def runJohn ():
             counter = 0
             while True:
-                await self.johnSocket.send (json.dumps (f'counter john {self.secondName} is {counter}'))
+                answer = await aioconsole.ainput ('John says WHAT?')
+                await self.johnSocket.send (json.dumps (f'counter john {self.secondName} is {counter} - {answer}'))
                 print (f'john {self.secondName}:', json.loads (await self.johnSocket.recv ()))
                 counter += 1
 
         async def runMary ():
             counter = 0
             while True:
+                await asyncio.sleep (0.5)
                 await self.marySocket.send (json.dumps (f'counter mary {self.secondName } is {counter}'))
                 print (f'mary {self.secondName}:', json.loads (await self.marySocket.recv ()))
                 counter -= 1
